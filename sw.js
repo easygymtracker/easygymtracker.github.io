6e3bb691dddc568ab5e2f324cb43cc367669497a
Víctor Gonzalo Cristóbal <victor.gonzalo@thenextpangea.com>
2026-05-06 12:41:17 +0200

    Update

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 // Cache strategy
 // Bump CACHE_VERSION whenever assets change.
-const CACHE_VERSION = "v6";
+const CACHE_VERSION = "v7";
 const CACHE_NAME = `gym-tracker-${CACHE_VERSION}`;
 
 // App-shell files to precache on install.
